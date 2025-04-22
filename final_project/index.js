b09b5196bcb934ad4ed1c5d33bc0df9c211f9b5d
João Vítor Macedo <vtcmjoao@gmail.com>
2025-04-21 22:13:03 -0400

    update authentication mechanism

--- final_project/index.js
+++ final_project/index.js
@@ -11,8 +11,17 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+  const token = req.header('Authorization').replace('Bearer ', '')
+  if (!token) return res.status(401).send('Access Denied: No Token Provided!')
+
+  try {
+    const verified = jwt.verify(token, 'fingerprint_customer')
+    req.user = verified
+    next()
+  } catch (err) {
+    res.status(400).send('Invalid Token')
+  }
+})
  
 const PORT =5000;
 
